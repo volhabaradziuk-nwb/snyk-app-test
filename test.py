9c291806fc92727f25dd5a9fb29541e5a146a78a
Volha Baradziuk <volha.baradziuk@nwboxed.com>
2025-05-08 13:08:19 +0200

    Snyk Code

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+user_input = input("Enter filename: ")
+with open(user_input, 'r') as file:  # Vulnerable to directory traversal
+    content = file.read()
+
+
 user_input = input("Enter your username: ")
 query = "SELECT * FROM users WHERE username = '" + user_input + "';"
 execute_query(query)
